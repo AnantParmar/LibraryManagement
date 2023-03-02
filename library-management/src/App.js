18fb5796e6e43ae5adaf04a28972ac04c1ed86bb
shubham <shubhamvaghela944@gmail.com>
2023-03-02 21:48:17 +0530

    add more commit

--- library-management/src/App.js
+++ library-management/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-//shubhm
+
+// shubham vaghela
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React js by shubham vaghela
         </a>
       </header>
     </div>
